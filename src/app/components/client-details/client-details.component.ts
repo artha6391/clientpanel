00c169566aca1189f5e10875dcb57093c5a26ff4
Artha <arthapotdar@gmail.com>
2021-07-31 17:19:20 +0530

    clientpanel part 1 done

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/Client';
+import { ClientService } from 'src/app/services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-client-details',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.clientService.getClient(this.id).subscribe(client => {
+      if(client !== null) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+
+      this.client = client;
+      console.log(this.client);
+    });
+  }
+
+  onDeleteClick() {
+    
   }
 
 }
