d515b2dd8d2d93240769c68e8ebcdcba556f73cf
Artha <arthapotdar@gmail.com>
2021-07-31 16:31:01 +0530

    new client form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCUEpWvDTEXoXe8IAQOAfsTUA7_yPEAjBY",
+    authDomain: "clientpanelprod-75328.firebaseapp.com",
+    projectId: "clientpanelprod-75328",
+    storageBucket: "clientpanelprod-75328.appspot.com",
+    messagingSenderId: "333548671084",
+    appId: "1:333548671084:web:df740c8f80c02324c3c849",
+    measurementId: "G-XCPSM4ZTSH"
+  }
 };
